32c0b041871150fbe9b6aadaf01c06c59478bf49
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-06-25 11:44:21 -0400

    Variables declared along with input and output statements

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CoreCalculator.java
@@ -0,0 +1,32 @@
+package com.zipcodewilmington.scientificcalculator;
+import java.util.Scanner;
+public class CoreCalculator {
+
+
+    public static void main (String[] args){
+        String userInput;
+        double digit1, digit2, result;
+        String sqr, sqrt, exp, clr;
+
+
+        System.out.println("Ent 1st digit");
+        System.out.println("Ent task : +, -, *, /, sqr, sqrt, exp");
+        System.out.println("Ent 2nd digit");
+
+        Scanner input = new Scanner(System.in);
+        digit1 = input.nextDouble();
+        userInput = input.next();
+        digit2 = input.nextDouble();
+
+        if (userInput == "+") {
+
+
+        }
+
+
+
+
+    }
+
+
+}
